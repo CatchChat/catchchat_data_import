db5211b79e33977d3a12ee3d799704943f90c550
David Ruan <ruanwz@gmail.com>
2014-10-27 09:44:42 +0800

    Squashed 'upstream/catchchat_server/' content from commit 545e9da
    git-subtree-dir: upstream/catchchat_server
    git-subtree-split: 545e9dafa286276a77cb6c0521d24e4cda00247a

---
+++ app/services/sms.rb
@@ -0,0 +1,12 @@
+require 'forwardable'
+class Sms
+  extend Forwardable
+  def_delegators :@provider, :send_sms
+  attr_accessor :options, :provider
+
+  def initialize(provider, options)
+    @options  = options
+    @provider = provider
+    provider.prepare(self)
+  end
+end
---
+++ spec/services_helper.rb
@@ -0,0 +1,11 @@
+ENV["qiniu_access_key"]="BBHE3ccYQ8VQhEIvZbJARrte1U3ic2Om6CW7mxvN"
+ENV["qiniu_secret_key"]="pQ5v3Bpg6AIx5hIXo9fXJgCLjgObH-DjKWOWU8dy"
+ENV["AWS_ACCESS_KEY_ID"]="AKIAOGBVMZAU5EZPGPIQ"
+ENV["AWS_SECRET_ACCESS_KEY"]="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"
+ENV["AWS_REGION"]='cn-north-1'
+ENV["AWS_Bucket"]='rails-test'
+ENV["upyun_username"]="david"
+ENV["upyun_password"]="coneonbe"
+ENV["upyun_form_api_secret"]="hY2fekHKoBJWcdTC1bL4T6mgN+A="
+ENV["luosimao_username"]="api"
+ENV["luosimao_apikey"]="key-4eb4b5a344a74b2d1a4671e32a2cc46f"
