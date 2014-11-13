ceb351a908b8f6fe39def7de281a3ebb63d32240
David Ruan <ruanwz@gmail.com>
2014-11-13 16:12:39 +0800

    Remove node api relating header in helper

--- lib/generator_helper.rb
+++ lib/generator_helper.rb
@@ -10,12 +10,6 @@ module GeneratorHelper
   end
   private
   def send_request(method, host, path, body=nil)
-    username = 'ruanwztest'
-    userid = '542a22ae4b44684f2ecb2398'
-    token = '6c9fa5fcd90f86d56fa271a9b80f649e8e4c327097707cb12d7262ce93537d3d'
-    password = 'ruanwztest'
-    encoded_auth_password = Base64.encode64("#{username}:#{password}")
-    encoded_auth_token = Base64.encode64("#{userid}:#{token}")
     conn = Faraday.new(:url => host) do |faraday|
       # faraday.request  :url_encoded             # form-encode POST params
       # faraday.response :logger                  # log requests to STDOUT
@@ -26,7 +20,7 @@ module GeneratorHelper
       # req.headers['X-CatchChatToken'] = encoded_auth_token
       # Below is only for node api
       # req.headers['X-CatchChatAuth'] = encoded_auth_password
-      req.body = body if method == :post
+      req.body = body if method == :post || method == :put
     end
   end
 end
